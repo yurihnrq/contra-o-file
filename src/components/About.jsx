c547317d2e1e6a19b48277317f350a539e0a27b3
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-01-17 20:20:52 -0300

    feat: About component

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+import artwork from "../images/undraw_cooking_lyxy.svg"
+import { about, aboutText, aboutImg } from "../styles/About.module.css"
+
+const About = () => {
+	return (
+		<section className={about} id="sobre">
+			<h2>
+				Sobre nós
+			</h2>
+			<article>
+				<div className={aboutText}>
+					<p>
+						Oferecemos comida vegana para você que segue esse estilo a tempos, mas não esquecemos de quem acabou de entrar nessa vida.
+					</p>
+					<p>
+						Trabalhamos somente com produtos orgânicos de agricultura familiar, incentivando a produção local.
+					</p>
+					<p>
+						Temos como objetivo a democratização do veganismo, temos pratos com preços que cabem no seu bolso.
+					</p>
+				</div>
+				<div className={aboutImg}>
+					<img src={artwork} alt="Imagem sobre nós" />
+				</div>
+			</article>
+		</section>
+	)
+}
+
+export default About;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,14 @@ import * as React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import LandingSection from "../components/LandingSection"
+import About from "../components/About"
+
 
 const IndexPage = () => (
 	<Layout>
 		<Seo title="Bem-vindo" />
 		<LandingSection />
-		Dale
+		<About />
 	</Layout>
 )
 
