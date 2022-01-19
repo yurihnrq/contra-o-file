46e52c42eaf51566c1cc6d9e6eee29633419df8d
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-01-18 22:15:47 -0300

    fix: button margin correction

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -9,7 +9,7 @@ const Order = () => {
 			<h2>Faça seu pedido</h2>
 			<div className={buttons}>
 				<a href="https://ifood.com.br" target="_blank" rel="norefferer">
-					<Button text={"Peça pelo iFood"} className={"mr-3 sm:mr-0"} />
+					<Button text={"Peça pelo iFood"} className={"mr-0 sm:mr-3"} />
 				</a>
 				<a href="https://whatsapp.com/?lang=pt_br" target="_blank" rel="norefferer">
 					<Button text={"Peça pelo WhatsApp"} className={"mt-3 sm:mt-0"} />
